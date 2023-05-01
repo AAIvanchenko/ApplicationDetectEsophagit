7b1ffcc10341e1c24be0164dfdff38303dd3120b
Анастасия Иванченко <89zazaza89@gmail.com>
2023-05-01 17:38:46 +0700

    create preprocessing img file

--- src/dataset.py
+++ src/dataset.py
@@ -2,6 +2,9 @@ from pathlib import Path
 import cv2
 import numpy as np
 
+from preprocessing_img import letterbox
+
+
 VID_FORMATS = ['mp4', 'mkv']
 
 class VideoToBatchImage:
@@ -86,33 +89,3 @@ class VideoToBatchImage:
         if self.padding:
             self.frame += self.padding
             self.cap.set(cv2.CAP_PROP_POS_FRAMES, self.padding)
-    
-def letterbox(im, new_shape=(640, 640), color=(0, 0, 0), scaleFill=False, scaleup=False):
-    # Resize and pad image while meeting stride-multiple constraints
-    shape = im.shape[:2]  # current shape [height, width]
-    if isinstance(new_shape, int):
-        new_shape = (new_shape, new_shape)
-
-    # Scale ratio (new / old)
-    r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
-    if not scaleup:  # only scale down, do not scale up (for better val mAP)
-        r = min(r, 1.0)
-
-    # Compute padding
-    # ratio = r, r  # width, height ratios
-    new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))
-    dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - new_unpad[1]  # wh padding
-    if scaleFill:  # stretch
-        dw, dh = 0.0, 0.0
-        new_unpad = (new_shape[1], new_shape[0])
-        # ratio = new_shape[1] / shape[1], new_shape[0] / shape[0]  # width, height ratios
-
-    dw /= 2  # divide padding into 2 sides
-    dh /= 2
-
-    if shape[::-1] != new_unpad:  # resize
-        im = cv2.resize(im, new_unpad, interpolation=cv2.INTER_LINEAR)
-    top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
-    left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
-    im = cv2.copyMakeBorder(im, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)  # add border
-    return im
