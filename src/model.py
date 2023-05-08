e3580b018527dbb1b6c13916e6f7d6f4b5770fcb
Анастасия Иванченко <89zazaza89@gmail.com>
2023-05-09 00:17:41 +0700

    Add model detection

--- src/model.py
+++ src/model.py
@@ -4,16 +4,16 @@ import torch
 
 PATH_WEIGHT_DETECTION = "E:/7/KPP/ApplicationDetectEsophagit/weights_model/detection/best_yolov8.pt"
 PATH_WEIGHT_CLASSIFICATION = "E:/7/KPP/ApplicationDetectEsophagit/weights_model/classification/densnet121_softmax_eph50.pt"
+CONF_DETECTION_MODEL = 0.6
+
 
 class ModelDetect(torch.nn.Module):
     def __init__(self):
         super(ModelDetect, self).__init__()
         self.model = YOLO(PATH_WEIGHT_DETECTION)
-        self.model.val()
 
-    def forward(self, img, conf=0.8):
-        # картику не нужно преобразовывать в torch после прочтения cv2
-        predict = self.model.predict(source=img, conf=conf)
+    def forward(self, img):
+        predict = self.model.predict(source=img, conf=CONF_DETECTION_MODEL)
         return predict
 
 
