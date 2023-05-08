fd6cff7802309628510ab7e2849d132b1bcafba2
Анастасия Иванченко <89zazaza89@gmail.com>
2023-05-08 13:37:23 +0700

    Refactor class ModelClassificationZline

--- src/model.py
+++ src/model.py
@@ -1,11 +1,11 @@
 from ultralytics import YOLO
 from torch import load
-import torch.nn as nn
+import torch
 
 PATH_WEIGHT_DETECTION = "E:/7/KPP/ApplicationDetectEsophagit/weights_model/detection/best_yolov8.pt"
 PATH_WEIGHT_CLASSIFICATION = "E:/7/KPP/ApplicationDetectEsophagit/weights_model/classification/densnet121_softmax_eph50.pt"
 
-class ModelDetect(nn.Module):
+class ModelDetect(torch.nn.Module):
     def __init__(self):
         super(ModelDetect, self).__init__()
         self.model = YOLO(PATH_WEIGHT_DETECTION)
@@ -17,16 +17,23 @@ class ModelDetect(nn.Module):
         return predict
 
 
-class ModelClassificationZLine(nn.Module):
+class ModelClassificationZLine(torch.nn.Module):
     def __init__(self):
         super(ModelClassificationZLine, self).__init__()
-        self.model = load(PATH_WEIGHT_CLASSIFICATION)
+        self.model = torch.load(PATH_WEIGHT_CLASSIFICATION)
+        self.device = torch.device("cuda") if torch.cuda.is_available else torch.device("cpu")
+    
+    def img_to_tensor(self, img):
+        img = img.copy()
+        img = img.transpose((2, 0, 1))
+        img = torch.from_numpy(img).to(torch.float32)
+        img /= 255  # 0 - 255 to 0.0 - 1.0
+        if len(img.shape) == 3:
+            img = img.unsqueeze(0)
+        return img
     
     def forward(self, img):
-        # картинка прочитана и переведена в торч
-        predict = self.model(img)
+        img = self.img_to_tensor(img).to(self.device)
+        predict = self.model(img).to(self.device)
         return predict
-    
-
-
     
