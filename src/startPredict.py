99185b974bca3097675a2d49c96e07d83eded596
Анастасия Иванченко <89zazaza89@gmail.com>
2023-05-11 14:53:10 +0700

    Refactor return in class StartPredict

--- src/startPredict.py
+++ src/startPredict.py
@@ -1,18 +1,11 @@
-import os
 from src.predict import PredictClassification, PredictDetection
 
-PATH_SAVE = '../prediction'
+PATH_SAVE = 'prediction'
 
 class StartPredict():
-    def __init__(self, path_video) -> None:
+    def __init__(self, path_video):
         self.pred_classification = PredictClassification(path_video=path_video)
 
-        if  len(self.pred_classification) <= 0:
-            return 'Не было найдено z-линии желудка'
-        else:
-            self.pred_detection = PredictDetection(PATH_SAVE, self.pred_classification)
-            # прошла детекция
-            if len(os.listdir(PATH_SAVE)) == 0:
-                return 'Не было обноружено болезни'
-            else:
-                return ''
+        if  len(self.pred_classification) > 0:
+            PredictDetection(PATH_SAVE, self.pred_classification)
+
